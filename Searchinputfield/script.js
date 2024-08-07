162dbaf47250b6c4308831e996426c292b33d4dc
Yasaswi159 <103161322+Yasaswi159@users.noreply.github.com>
2024-08-07 09:20:58 +0530

    Add files via upload

---
+++ Searchinputfield/script.js
@@ -0,0 +1,17 @@
+// Optional: Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
+
+function selectCategory(category) {
+    var dropdownButton = document.getElementById('dropdownButton');
+    dropdownButton.innerHTML = category + ' <i class="fas fa-chevron-down"></i>';
+}
